b4917ff65b58fec3893e9ad5675af06bbb7da526
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-01-31 12:50:25 +0530

    Add files via upload

---
+++ practice/feetinch with new method.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+class Cla
+{
+	public:
+	int feet,inch,n,sum=0,sum1=0,i,j;
+	
+	void input()
+	{
+		cout<<"Enter the size of n : ";
+		cin>>n;
+		
+		for(i=0;i<n;i++)
+		{
+			cout<<"Enter the size of feet : ";
+			cin>>feet;
+			cout<<"Enter the size of inch : ";
+			cin>>inch;
+			sum=sum+feet;
+			sum1=sum1+inch;
+		}
+		sum=sum+(sum1/12);
+		sum1=sum1%12;
+		cout<<" "<<sum<<endl;
+		cout<<" "<<sum1;
+	}
+		
+		
+	
+};
+main()
+{
+	Cla m;
+	m.input();
+	
+	
+}
