a88934b3c7ef2360bc8c29d3a368d825c1c702e4
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-01-31 16:12:13 +0530

    Update Customer.cpp

--- Data Encapsulation/Customer.cpp
+++ Data Encapsulation/Customer.cpp
@@ -11,47 +11,44 @@ using namespace std;
 class Cla
 {
 	int cust_id,cust_age,cust_simcard_validity;
-	int cust_mobile_number;
-	string cust_name,cust_city,cust_telecom_brand_name,line;
+	
+	string cust_name,cust_city,cust_telecom_brand_name,cust_mobile_number,line;
 	
 	public:
 		void input(int a)
 		{
-			
-				cout<<""<<endl;
-		    	cout<<""<<"Enter the detail of customer :"<<a<<" "<<"Customer"<<endl;
-		    	cout<<"Enter the customer id ";
-		    	cin>>cust_id;
-		    	getline(cin,line);
-		    	cout<<"Enter the customer name : ";
-		    	getline(cin,cust_name);
-		    	cout<<"Enter the customer age ";
-		    	cin>>cust_age;
-		    	getline(cin,line);
-		    	cout<<"Enter the customer city : ";
-		    	getline(cin,cust_city);
-		    	cout<<"Enter the customer mobile number ";
-		    	cin>>cust_mobile_number;
-		    	cout<<"Enter the customer cust_simcard_validity year ";
-		    	cin>>cust_simcard_validity;
-		    	getline(cin,line);
-		    	cout<<"Enter the cust_telecom_brand_name : ";
-		    	getline(cin,cust_telecom_brand_name);
-		    	
-		    	
-		    	
-		    
-			
+			cout<<""<<endl;
+		   	cout<<""<<"Enter the detail of customer :"<<a<<" "<<"Customer"<<endl;
+		    cout<<"Enter the customer id ";
+		    cin>>cust_id;
+		    getline(cin,line);
+		    cout<<"Enter the customer name : ";
+		    getline(cin,cust_name);
+		    getline(cin,line);
+		    cout<<"Enter the customer city : ";
+		    getline(cin,cust_city);
+		    cout<<"Enter the customer age ";
+		    cin>>cust_age;
+		    getline(cin,line);
+		    cout<<"Enter the customer mobile number ";
+		    getline(cin,cust_mobile_number);
+		    cout<<"Enter the customer cust_simcard_validity year ";
+		    cin>>cust_simcard_validity;
+		    getline(cin,line);
+		    cout<<"Enter the cust_telecom_brand_name : ";
+		    getline(cin,cust_telecom_brand_name);
+		    cout<<" "<<endl;
+	
 		}
 		
 		void output(int a)
 		{
 			
-			cout<<"this are detail of :"<<a<<"Customer"<<endl;
+			cout<<"this are detail of :"<<a<<""<<"Customer "<<endl;
 			cout<<"cust_id : "<<cust_id<<endl;
 			cout<<"cust_name : "<<cust_name<<endl;
 			cout<<"cust_age : "<<cust_age<<endl;
-			cout<<"cust_simcard_validity : "<<cust_simcard_validity<<endl;
+			cout<<"cust_simcard_validity : "<<cust_simcard_validity<<"year"<<endl;
 			cout<<"cust_mobile_number : "<<cust_mobile_number<<endl;
 			cout<<"cust_city : "<<cust_city<<endl;
 			cout<<"cust_telecom_brand_name : "<<cust_telecom_brand_name<<endl;
