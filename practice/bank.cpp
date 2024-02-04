5ca168376aea077cc1f6bb7c3be22e4f6416c8ae
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-02-04 14:51:25 +0530

    Update bank.cpp

--- practice/bank.cpp
+++ practice/bank.cpp
@@ -3,25 +3,48 @@ using namespace std;
 
 class Bank
 {
-	int balance=10000,withdraw_amount,deposite_amount;
+	int balance,withdraw_amount,deposite_amount;
 	public:
 	
 	void input()
 	{
+		
+		balance=10000;
 		cout<<"Balance : "<<balance<<endl;
+		start:
 		cout<<"Enter the deposite amount : "<<endl;
 		cin>>deposite_amount;
+		if(deposite_amount>0)
+		{
+			balance+=deposite_amount;
+			cout<<"Total balance :"<<balance<<endl;
+			
+			output();	
+		}
+		else
+		{
+			cout<<"Enter the positive number amount : "<<endl;
+			goto start;
+		}
 		
-		balance+=deposite_amount;
-		cout<<"Total balance :"<<balance<<endl;
 	}
 	void output()
 	{
+		sta:
 		cout<<"Enter the withdraw amount : ";
 		cin>>withdraw_amount;
+		if(withdraw_amount>0)
+		{
+			balance-=withdraw_amount;
+			cout<<"current balance :"<<balance;
+			
+		}
+		else
+		{
+			cout<<"Enter the positive number amount : "<<endl;
+			goto sta;
+		}
 		
-		balance-=withdraw_amount;
-		cout<<"current balance :"<<balance;
 	}
 	
 };
@@ -29,6 +52,4 @@ main()
 {
 	Bank b;
 	b.input();
-	b.output();
-	
 }
