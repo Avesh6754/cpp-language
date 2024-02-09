f64e058456fdaeb8385dadc80fd5dd72d9e1644f
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-02-09 12:36:06 +0530

    Update cube.cpp

--- inheritance/cube.cpp
+++ inheritance/cube.cpp
@@ -1,41 +1,39 @@
+//Questions:
+//Q.1 Write a Program to find the sum of all three numberâ€™s cubes by implementing single-level inheritance: Class X->Class Y
+//- Class X has the following members: a, b & c attributes in integer data type
+//- Class Y has the following members: setData() and getData() methods
 #include<iostream>
 using namespace std;
-/*Q.1 Write a Program to find the sum of all three number’s cubes by implementing single-level inheritance: Class X->Class Y
-- Class X has the following members: a, b & c attributes in integer data type
-- Class Y has the following members: setData() and getData() methods*/
 
-class A
+class X
 {
-	protected :
-		int a;
+	protected:
+		int a,b,c;
+	
 };
-class B:public A
+class Y :public X
 {
-		protected :
-		int b;
-};
-class C :public B
-{
+	int sum=0;
 	public:
-	int c,sum=0;
-	void set()
-	{
-		cout<<"Enter the value of a : ";
-		cin>>a;
-		cout<<"Enter the value of b : ";
-		cin>>b;
-		cout<<"Enter the value of c : ";
-		cin>>c;
-	}
-	void get()
-	{
-		sum=(a*a*a)+(b*b*b)+(c*c*c);
-		cout<<"Sum of cubes : "<<sum;
-	}
+		void setData()
+		{
+			cout<<"Emter the value of a : ";
+			cin>>a;
+			cout<<"Emter the value of b : ";
+			cin>>b;
+			cout<<"Emter the value of c : ";
+			cin>>c;
+			
+		}
+		void getData()
+		{
+			sum=(a*a*a)+(b*b*b)+(c*c*c);
+			cout<<"Sum = "<<sum;
+		}
 };
 main()
 {
-	C c1;
-	c1.set();
-	c1.get();
+	Y y1;
+	y1.setData();
+	y1.getData();
 }
